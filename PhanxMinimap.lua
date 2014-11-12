01f18adaecb16f02fc92a66aba3d7713c276a773
Phanx <addons@phanx.net>
2014-11-12 05:08:11 -0800

    Post SVN->Git cleanup

--- PhanxMinimap.lua
+++ PhanxMinimap.lua
@@ -2,13 +2,6 @@
 	PhanxMinimap
 	Just another basic minimap modification.
 	Copyright (c) 2008-2014 Phanx <addons@phanx.net>. All rights reserved.
-
-	Please DO NOT upload this addon to other websites, or post modified
-	versions of it. However, you are welcome to include a copy of it
-	WITHOUT CHANGES in compilations posted on Curse and/or WoWInterface.
-	You are also welcome to use any/all of its code in your own addon, as
-	long as you do not use my name or the name of this addon ANYWHERE in
-	your addon, including its name, outside of an optional attribution.
 ----------------------------------------------------------------------]]
 
 local SCALE = 1.5
